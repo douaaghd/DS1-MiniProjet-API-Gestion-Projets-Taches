f5031a7e679ae1b81bd4974f22270b8e83b2ed44
douaa <ghidaouidouaa3@gmail.com>
2025-12-01 20:28:28 +0100

    Création contrôleur et routes projets

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
 require("dotenv").config();
 
 
@@ -12,7 +13,7 @@ app.use(express.json());
 
 
 app.use("/api/users",require("./routes/userRoutes"));
-app.use("/api/user",require("./routes/projectRoutes"));
+app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks",require("./routes/taskRoutes"));
 
 
